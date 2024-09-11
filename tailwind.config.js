257d06dbed25e3326a3cb484e2b36730e108d2a8
unknown <dl.kokhtashvili@gmail.com>
2024-09-12 00:59:58 +0400

    Add FiraGo font to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
+    extend: {
+      fontFamily: {
+        firaGo: ["FiraGO", "sans-serif"],
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        bold: 700,
+      },
+    },
+
     screens: {
       xl: "1280px",
     },
